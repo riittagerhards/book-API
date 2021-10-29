da6d2ce3f17a221bc61ee8a9e64c86dbb7425ac8
riittagerhards <rgerhards@outlook.com>
2021-10-29 10:35:26 +0200

    🔧 Finish setup for the project

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,25 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
+
+import { connectDatabase, getBookCollection } from './utils/database';
+
+if (!process.env.MONGODB_URI) {
+  throw new Error('No MongoDB URI dotenv variable');
+}
 
 const app = express();
 const port = 3000;
+
+app.use(express.json());
 
 app.get('/', (_req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+connectDatabase(process.env.MONGODB_URI).then(() =>
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  })
+);
